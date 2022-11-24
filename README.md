d864e71767cf41cb0b49dfee67f68d59b003aade
Daniel George <dan20geo20@gmail.com>
2022-11-23 17:04:04 -0800

    explore

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Lab 8 - Starter
 
 1) I would put it as a github action whenever code is pushed so that it will run automatically. This way we can ensure tests run when reviewing pull request. 
-2) No that's a unit test.s
+2) No that's a unit test.
+3) NO because that is a functionality that involves many pieces of code/functions. You want unit tests for each. That would be an end to end test. 
+4) Yes because that's a single functionality that tests a single area of code.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,81 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Phone numbers
+test('phone number true', () => {
+    expect(functions.isPhoneNumber("(408) 594-8527")).toBe(true);
+});
+
+test('phone number true 2', () => {
+    expect(functions.isPhoneNumber("800-594-8527")).toBe(true);
+});
+
+test('phone number false', () => {
+    expect(functions.isPhoneNumber("408-594-852")).toBe(false);
+});
+
+test('phone number false 2', () => {
+    expect(functions.isPhoneNumber("(408)-594-852")).toBe(false);
+});
+
+test('email true', () => {
+    expect(functions.isEmail("d1george@ucsd.edu")).toBe(true);
+});
+
+test('email true 2', () => {
+    expect(functions.isEmail("d1george@gmail.com")).toBe(true);
+});
+
+test('email false', () => {
+    expect(functions.isEmail("d1georgeucsd.edu")).toBe(false);
+});
+
+test('email false 2', () => {
+    expect(functions.isEmail("d1george")).toBe(false);
+});
+
+test('password true', () => {
+    expect(functions.isStrongPassword("DanPass314")).toBe(true);
+});
+test('password true 2', () => {
+    expect(functions.isStrongPassword("DanPass")).toBe(true);
+});
+
+test('password false', () => {
+    expect(functions.isStrongPassword("Da")).toBe(false);
+});
+test('password false 2', () => {
+    expect(functions.isStrongPassword("Daas;dlkjlaksjdlgkjasdlkgj")).toBe(false);
+});
+
+test('date true', () => {
+    expect(functions.isDate("10/10/2020")).toBe(true);
+});
+
+test('date true 2', () => {
+    expect(functions.isDate("10/1/2020")).toBe(true);
+});
+
+test('date false', () => {
+    expect(functions.isDate("10//2020")).toBe(false);
+});
+
+test('date false 2', () => {
+    expect(functions.isDate("10/11/202")).toBe(false);
+});
+
+test('hex color true', () => {
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+});
+
+test('hex color true 2', () => {
+    expect(functions.isHexColor("F00FFF")).toBe(true);
+});
+
+test('hex color false', () => {
+    expect(functions.isHexColor("F00FF")).toBe(false);
+});
+
+test('hex color false 2', () => {
+    expect(functions.isHexColor("#F00FF")).toBe(false);
+});
